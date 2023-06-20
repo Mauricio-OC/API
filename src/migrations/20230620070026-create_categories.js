c666d11309924db0e904325c1be3c89f6eddc88c
Mauricio-OC <murisc0@hotmail.com>
2023-06-20 06:16:29 -0300

    migrations

--- src/migrations/20230620070026-create_categories.js
+++ src/migrations/20230620070026-create_categories.js
@@ -1,21 +1,20 @@
-'use strict';
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('categories', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primayKey: true,
+      type: Sequelize.INTEGER,
+  },
+  name: {
+    allowNull: false,
+    type: Sequelize.STRING,
+  },
+});
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+    await queryInterface.dropTable('categories');
+  },
 };
--- src/migrations/20230620070402-create_posts_categories.js
+++ src/migrations/20230620070402-create_posts_categories.js
@@ -1,21 +1,28 @@
-'use strict';
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('posts_categories', {
+      post_id: {
+        allowNull:false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'blog_posts',
+          key: 'id',
+        },
+        primaryKey: true,
+      },
+      category_id: {
+        allowNull:false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'categories',
+          key: 'id',
+        },
+        primaryKey: true,
+      }
+    })
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('posts_categories');
   }
 };
